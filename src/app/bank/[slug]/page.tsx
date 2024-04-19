cd473f878f925679c787279c0efc3bf046740b7f
Developer1@Saharsh@123 <developer1.saharsh.software@gmail.com>
2024-04-19 19:15:10 +0530

    build-minor-issue

--- src/app/bank/[slug]/page.tsx
+++ src/app/bank/[slug]/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({
 }) {
   return (
     <>
-      <ShowAuctionList isBankRoute={true} />
+      <ShowAuctionList />
     </>
   );
 }
--- src/app/location/[slug]/page.tsx
+++ src/app/location/[slug]/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({
 }) {
   return (
     <>
-      <ShowAuctionList isLocationRoute={true} />
+      <ShowAuctionList />
     </>
   );
 }
