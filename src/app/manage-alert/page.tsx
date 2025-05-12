fe40d9d5905624fcaf3ac088a650c7f5f5bf12f8
Saurabh Shukla <saurabhshukla3107@gmail.com>
2025-05-12 14:29:51 +0530

    Improvements in page performance

--- src/app/manage-alert/page.tsx
+++ src/app/manage-alert/page.tsx
@@ -1,6 +1,15 @@
 import ManageAlert from "@/components/templates/ManageAlert";
+import { COOKIES } from "@/shared/Constants";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
+  const cookieStore = cookies();
+  const token = cookieStore.get(COOKIES.TOKEN_KEY)?.value || null; // Replace with your actual cookie name
+
+  if (!token) {
+    return redirect("/login");
+  }
   return (
     <>
       <ManageAlert />
