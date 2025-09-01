bdd1ef2456d4be54c61c92933576ffd0ac2f77be
Saurabh Shukla <saurabhshukla3107@gmail.com>
2025-09-01 14:25:51 +0530

    Refactor BlurredFieldWrapper and AuctionDetail2 components to include an icon in the blurred field and improve image carousel functionality

--- src/components/atoms/BlurredFieldWrapper.tsx
+++ src/components/atoms/BlurredFieldWrapper.tsx
@@ -2,17 +2,22 @@
 import React from "react";
 import AuthModal from "../ modals/AuthModal";
 import useModal from "@/hooks/useModal";
+import { Eye } from 'lucide-react';
 
 interface IBlurredFieldWrapperProps {
   children: React.ReactNode;
   blurText?: string;
   isBlurred?: boolean;
+  hasImageCarousel?: boolean;
+  icon?: React.ReactNode;
 }
 
 const BlurredFieldWrapper: React.FC<IBlurredFieldWrapperProps> = ({
   children,
   blurText = "Login to view",
   isBlurred,
+  hasImageCarousel = false,
+  icon,
 }) => {
   const { hideModal, openModal, showModal } = useModal();
 
@@ -35,9 +40,12 @@ const BlurredFieldWrapper: React.FC<IBlurredFieldWrapperProps> = ({
           <div className="bg-white/70">
             <button
               onClick={handleBlurClick}
-              className="absolute inset-0 flex items-center justify-center cursor-pointer  link link-primary font-semibold underline rounded w-fit h-fit m-auto"
+              className="absolute inset-0 flex items-center justify-center cursor-pointer link link-primary font-semibold underline rounded w-fit h-fit m-auto"
             >
-              {blurText}
+              <div className="flex items-center gap-2">
+                {icon && <Eye className="h-4 w-4" />}
+                <span>{blurText}</span>
+              </div>
             </button>
           </div>
         )}
