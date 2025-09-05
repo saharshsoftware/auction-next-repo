f2215b2f3abaa0a133b721cce2f38fcc5e81d83f
Saurabh Shukla <saurabhshukla3107@gmail.com>
2025-09-05 19:41:56 +0530

    Add additional redirects for property types in Next.js configuration
    - Introduced new redirect rules for locations, categories, banks, and individual property types.

--- next.config.mjs
+++ next.config.mjs
@@ -67,6 +67,26 @@ const nextConfig = {
         source: "/assets",
         destination: `${process.env.NEXT_PUBLIC_DOMAIN_BASE_URL}/property-types`,
         permanent: false,
+      },
+      {
+        source: "/locations/:slug/types/:path*",
+        destination: `${process.env.NEXT_PUBLIC_DOMAIN_BASE_URL}/locations/:slug/property-types/:path*`,
+        permanent: false,
+      },
+      {
+        source: "/categories/:slug/types/:path*",
+        destination: `${process.env.NEXT_PUBLIC_DOMAIN_BASE_URL}/categories/:slug/property-types/:path*`,
+        permanent: false,
+      },
+      {
+        source: "/banks/:slug/types/:path*",
+        destination: `${process.env.NEXT_PUBLIC_DOMAIN_BASE_URL}/banks/:slug/property-types/:path*`,
+        permanent: false,
+      },
+      {
+        source: "/types/:slug",
+        destination: `${process.env.NEXT_PUBLIC_DOMAIN_BASE_URL}/property-types/:slug`,
+        permanent: false,
       }
     ];
   },
