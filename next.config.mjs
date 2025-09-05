5ce6b2fc5dd21b702049c78d49bab1806cfc4f22
Saurabh Shukla <saurabhshukla3107@gmail.com>
2025-09-05 18:43:38 +0530

    Update redirect destination for "/types" in Next.js configuration to point to "/assets".

--- next.config.mjs
+++ next.config.mjs
@@ -60,7 +60,7 @@ const nextConfig = {
       },
       {
         source: "/types",
-        destination: `${process.env.NEXT_PUBLIC_DOMAIN_BASE_URL}/property-types`,
+        destination: `${process.env.NEXT_PUBLIC_DOMAIN_BASE_URL}/assets`,
         permanent: true,
       }
     ];
