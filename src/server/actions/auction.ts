3861fc92b3fe162b2336a0511ea7b7314ad87393
Saurabh Shukla <84230669+Sam-Tennyson@users.noreply.github.com>
2025-09-11 11:07:21 +0530

    Add sortOrder property to IAssetType and ICategoryCollection interfac… (#102)
    * Add sortOrder property to IAssetType and ICategoryCollection interfaces; refactor data fetching in category pages to utilize caching for improved performance and consistency. Update asset filtering logic to incorporate sortOrder
    
    * Update SEO metadata generation to use site logo for og:image

--- src/server/actions/auction.ts
+++ src/server/actions/auction.ts
@@ -178,17 +178,15 @@ export const getCategoryBoxCollectionBySlug = async (props: {
     const { slug } = props;
     const filter = `?filters[slug][$eq]=${slug}`;
     const URL = API_BASE_URL + API_ENPOINTS.CATEGORY_BOX_COLLETIONS + filter;
+    // console.log(URL, "category-url-slug");
     const response = await fetch(URL, {
       next: { revalidate: FILTER_API_REVALIDATE_TIME },
-      method: "GET",
       headers: { "Content-Type": "application/json" },
     });
-    if (!response.ok) {
-      throw new Error("Failed to fetch category by slug");
-    }
-    const result = await response.json();
-    const sendResponse = sanitizeStrapiData(result?.data) as unknown;
-    return sendResponse;
+    if (!response.ok) throw new Error("Failed to fetch category by slug");
+    const json = await response.json();
+    const result = sanitizeStrapiData(json?.data);
+    return result;
   } catch (e) {
     console.log(e, "category-slug error category-box");
   }
@@ -276,6 +274,7 @@ export const fetchAssetType = async () => {
       "name",
       "slug",
       "pluralizeName",
+      "sortOrder",
     ]);
     const filter = `?sort[0]=name:asc&${requiredkeys}&populate=category`;
     const URL = API_BASE_URL + API_ENPOINTS.ASSET_TYPES + `${filter}`;
