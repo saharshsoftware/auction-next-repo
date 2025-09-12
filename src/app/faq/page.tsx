4b78158c99fbfd1387c55e502723bf14dd4d3dcb
Saurabh Shukla <saurabhshukla3107@gmail.com>
2025-09-12 12:16:05 +0530

    Update FAQ page and data fetching logic
    - Set revalidation time to 1 hour and enforce static rendering for the FAQ page.
    - Modify the FAQ data fetching URL to include pagination parameters for improved data retrieval.

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,6 +1,9 @@
 import FaqPage  from '@/components/templates/FaqPage';
 import { getFaqData } from '@/server/actions/footer';
 
+export const revalidate = 3600; // 1 hour
+export const dynamic = 'force-static';
+
 export default async function Page() {
   const res = await getFaqData();
   const faqData = res || []
