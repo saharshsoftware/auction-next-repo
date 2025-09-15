e2224acab178f55fa25ad233cde721830942eb61
Saurabh Shukla <saurabhshukla3107@gmail.com>
2025-09-15 12:43:03 +0530

    Refactor About Us page to use new AboutPage component
    - Removed old markdown rendering logic and replaced it with the AboutPage component for a more structured layout.
    - Introduced a new AboutPage component that includes detailed sections about the company's services and approach.
    - Enhanced the overall design and user experience of the About Us section.

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,43 +1,8 @@
 import React from "react";
-import MarkdownIt from "markdown-it";
-import { getAboutUsData } from "@/server/actions/footer";
+import {AboutPage} from "@/components/templates/AboutPage";
+
+export const dynamic = 'force-static';
 
 export default async function Page() {
-  const res = await getAboutUsData();
-  const renderMarkdown = (markdown: any) => {
-    const md = new MarkdownIt({
-      html: true,
-      linkify: true,
-    });
-    return md.render(markdown);
-  };
-
-  if (res) {
-    return (
-      <section className="common-section">
-        <div className="my-4">
-          <div className="flex flex-col gap-4 ">
-            <div
-              className="text-left text-base leading-[30px] font-normal relative self-stretch danger-blog-class"
-              dangerouslySetInnerHTML={{
-                __html: renderMarkdown(res?.mission ?? ""),
-              }}
-            ></div>
-          </div>
-        </div>
-        <div className="my-4">
-          <div className="flex flex-col gap-4 ">
-            <div
-              className="text-left text-base leading-[30px] font-normal relative self-stretch danger-blog-class"
-              dangerouslySetInnerHTML={{
-                __html: renderMarkdown(res?.vision ?? ""),
-              }}
-            ></div>
-          </div>
-        </div>
-      </section>
-    );
-  }
+  return <AboutPage />
 }
-
-export const revalidate = 0;
